e0a9a6b35d62e4a00e65aa66416693276dfdd4b3
ListenerJubatus <superfox436@gmail.com>
2016-05-10 20:39:26 -0600

    build out, screenselectmusic tweaks

---
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -0,0 +1,5 @@
+local t = Def.ActorFrame {};
+	
+	t[#t+1]= use_newfield_actor()
+
+return t
--- Graphics/ScreenWithMenuElements Footer.lua
+++ Graphics/ScreenWithMenuElements Footer.lua
@@ -1,7 +1,7 @@
 local t = Def.ActorFrame {};
 
 t[#t+1] = Def.Quad {
-	InitCommand=cmd(vertalign,bottom;zoomto,SCREEN_WIDTH,64;diffuse,color("#351526"));
+	InitCommand=cmd(vertalign,bottom;zoomto,SCREEN_WIDTH,55;diffuse,color("#351526"));
 };
 
 return t;
