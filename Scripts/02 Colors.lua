9b7157200685338ac030f1e2d2bbde2a6491e7d0
ListenerJubatus <superfox436@gmail.com>
2016-05-13 20:03:06 -0600

    Blue and Pink

---
+++ Scripts/02 Colors.lua
@@ -0,0 +1,68 @@
+ModeIconColors = {
+	Normal		= color("#FFEE00"), -- yellow
+	Rave		= color("#db93ff"), -- violet
+	Nonstop		= color("#5ca9ff"), -- blue
+	Oni			= color("#00f1e2"), -- cyan
+	Endless		= color("#b4c3d2"), -- steel
+}
+
+GameColor = {
+	PlayerColors = {
+		PLAYER_1 = color("#437CE7"),
+		PLAYER_2 = color("#EB3F8C"),
+	},
+	PlayerDarkColors = {
+		PLAYER_1 = color("#EB3F8C"),
+		PLAYER_2 = color("#89385A"),
+	},
+	Difficulty = {
+		--[[ These are for 'Custom' Difficulty Ranks. It can be very  useful
+		in some cases, especially to apply new colors for stuff you
+		couldn't before. (huh? -aj) ]]
+		Beginner	= color("#ff32f8"),			-- light cyan
+		Easy		= color("#2cff00"),			-- green
+		Medium		= color("#fee600"),			-- yellow
+		Hard		= color("#ff2f39"),			-- red
+		Challenge	= color("#1cd8ff"),			-- light blue
+		Edit		= color("0.8,0.8,0.8,1"),	-- gray
+		Couple		= color("#ed0972"),			-- hot pink
+		Routine		= color("#ff9a00"),			-- orange
+		--[[ These are for courses, so let's slap them here in case someone
+		wanted to use Difficulty in Course and Step regions. ]]
+		Difficulty_Beginner	= color("#ff32f8"),		-- purple
+		Difficulty_Easy		= color("#2cff00"),		-- green
+		Difficulty_Medium	= color("#fee600"),		-- yellow
+		Difficulty_Hard		= color("#ff2f39"),		-- red
+		Difficulty_Challenge	= color("#1cd8ff"),	-- light blue
+		Difficulty_Edit 	= color("0.8,0.8,0.8,1"),		-- gray
+		Difficulty_Couple	= color("#ed0972"),				-- hot pink
+		Difficulty_Routine	= color("#ff9a00")				-- orange
+	},
+	Stage = {
+		Stage_1st	= color("#00ffc7"),
+		Stage_2nd	= color("#58ff00"),
+		Stage_3rd	= color("#f400ff"),
+		Stage_4th	= color("#00ffda"),
+		Stage_5th	= color("#ed00ff"),
+		Stage_6th	= color("#73ff00"),
+		Stage_Next	= color("#73ff00"),
+		Stage_Final	= color("#ff0707"),
+		Stage_Extra1	= color("#fafa00"),
+		Stage_Extra2	= color("#ff0707"),
+		Stage_Nonstop	= color("#FFFFFF"),
+		Stage_Oni	= color("#FFFFFF"),
+		Stage_Endless	= color("#FFFFFF"),
+		Stage_Event	= color("#FFFFFF"),
+		Stage_Demo	= color("#FFFFFF")
+	},
+	Judgment = {
+		JudgmentLine_W1		= color("#bfeaff"),
+		JudgmentLine_W2		= color("#fff568"),
+		JudgmentLine_W3		= color("#a4ff00"),
+		JudgmentLine_W4		= color("#34bfff"),
+		JudgmentLine_W5		= color("#e44dff"),
+		JudgmentLine_Held	= color("#FFFFFF"),
+		JudgmentLine_Miss	= color("#ff3c3c"),
+		JudgmentLine_MaxCombo	= color("#ffc600")
+	},
+}
