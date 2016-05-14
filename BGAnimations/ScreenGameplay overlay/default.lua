4b9129315ab58bf047d7488090026b05df6f6743
ListenerJubatus <superfox436@gmail.com>
2016-05-14 00:52:16 -0600

    Move lifebar away from top of screen

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -2,12 +2,12 @@ local t = Def.ActorFrame {};
 	--P1
 	t[#t+1] = LoadActor("_lifep1") .. {
 		InitCommand=cmd(visible,GAMESTATE:IsHumanPlayer(PLAYER_1);x,Center1Player() and SCREEN_CENTER_X or THEME:GetMetric(Var "LoadingScreen","PlayerP1OnePlayerOneSideX");y,SCREEN_TOP-40);
-		OnCommand=cmd(addy,90);
+		OnCommand=cmd(addy,90+15);
 	};
 
 	t[#t+1] = LoadActor("_lifep2") .. {
 		InitCommand=cmd(visible,GAMESTATE:IsHumanPlayer(PLAYER_2);x,Center1Player() and SCREEN_CENTER_X or THEME:GetMetric(Var "LoadingScreen","PlayerP2OnePlayerOneSideX");y,SCREEN_TOP-40);
-		OnCommand=cmd(addy,90);
+		OnCommand=cmd(addy,90+15);
 	};
 
 return t
--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -2,12 +2,12 @@ local t = Def.ActorFrame {};
 	
 	t[#t+1] = LoadActor(THEME:GetPathG("LifeMeter", "p1 under")) .. {
 		InitCommand=cmd(visible,GAMESTATE:IsHumanPlayer(PLAYER_1);x,Center1Player() and SCREEN_CENTER_X or THEME:GetMetric(Var "LoadingScreen","PlayerP1OnePlayerOneSideX");y,SCREEN_TOP-40);
-		OnCommand=cmd(addy,90);
+		OnCommand=cmd(addy,90+15);
 	};
 
 	t[#t+1] = LoadActor(THEME:GetPathG("LifeMeter", "p2 under")) .. {
 		InitCommand=cmd(visible,GAMESTATE:IsHumanPlayer(PLAYER_2);x,Center1Player() and SCREEN_CENTER_X or THEME:GetMetric(Var "LoadingScreen","PlayerP2OnePlayerOneSideX");y,SCREEN_TOP-40);
-		OnCommand=cmd(addy,90);
+		OnCommand=cmd(addy,90+15);
 	};
 	
 	t[#t+1]= use_newfield_actor()
