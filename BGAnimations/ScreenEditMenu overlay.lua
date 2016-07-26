caff3dc0f3c9e5f14c4929c46f63e06aae684ce7
ListenerJubatus <superfox436@gmail.com>
2016-07-26 11:22:26 -0600

    layout tweak

--- BGAnimations/ScreenEditMenu overlay.lua
+++ BGAnimations/ScreenEditMenu overlay.lua
@@ -4,7 +4,7 @@ local steps_type_item_space= _screen.h*.06
 
 local steps_display= setmetatable({disable_wrapping= true}, item_scroller_mt)
 local function steps_transform(self, item_index, num_items)
-	self.container:xy((item_index-.5) * steps_item_space, 15)
+	self.container:xy((item_index-.5) * steps_item_space, 19)
 end
 local function stype_transform(self, item_index, num_items)
 	self.container:y((item_index-1) * steps_type_item_space)
@@ -12,7 +12,7 @@ end
 local stype_item_mt= edit_pick_menu_steps_display_item(
 	stype_transform, Def.BitmapText{
 		Font= "Common Condensed", InitCommand= function(self)
-			self:zoom(.75):horizalign(left)
+			self:zoom(0.8):horizalign(left):shadowlength(1)
 		end,
 		SetCommand= function(self, param)
 			self:settext(THEME:GetString("LongStepsType", ToEnumShortString(param.stype)))
@@ -20,7 +20,7 @@ local stype_item_mt= edit_pick_menu_steps_display_item(
 																 },
 	steps_transform, Def.BitmapText{
 		Font= "Common Fallback Font", InitCommand= function(self)
-			self:zoom(.75)
+			self:zoom(0.75):shadowlength(1)
 		end,
 		SetCommand= function(self, param)
 			self:settext(param.steps:GetMeter())
@@ -47,7 +47,7 @@ local title_y= bpm_y
 local artist_x= title_x
 local artist_y= length_y
 local steps_display_x= title_x
-local steps_display_y= title_y + (spacer * 1.55)
+local steps_display_y= title_y + (spacer * 1.2)
 local steps_display_items= (_screen.h - steps_display_y) / steps_type_item_space
 
 local menu_params= {
@@ -73,7 +73,7 @@ local menu_params= {
 		item_mt= cons_option_item_mt, item_params= {
 			text_commands= {
 				Font= "Common Fallback Font", OnCommand= function(self)
-					self:diffuse(color("#452429")):diffusealpha(0):decelerate(0.2):diffusealpha(1)
+					self:diffuse(color("#452429")):diffusealpha(0):decelerate(0.2):diffusealpha(1):shadowlength(1)
 				end,
 				OffCommand=function(self)
 					self:smooth(0.3):diffusealpha(0)
@@ -82,7 +82,7 @@ local menu_params= {
 			text_width= .7,
 			value_text_commands= {
 				Font= "Common Fallback Font", OnCommand= function(self)
-					self:diffuse(color("#6E2C34")):diffusealpha(0):decelerate(0.2):diffusealpha(1)
+					self:diffuse(color("#6E2C34")):diffusealpha(0):decelerate(0.2):diffusealpha(1):shadowlength(1)
 				end,
 				OffCommand=function(self)
 					self:smooth(0.3):diffusealpha(0)
@@ -155,7 +155,7 @@ local frame= Def.ActorFrame{
 	},
 	Def.BitmapText{
 		Name= "length", Font= "Common Fallback Font", InitCommand= function(self)
-			self:xy(length_x, length_y):horizalign(right):zoom(.75):diffuse(color("#2E0F0F"))
+			self:xy(length_x, length_y):horizalign(right):zoom(.75):diffuse(color("#2E0F0F")):shadowlength(1)
 		end,
 		edit_menu_selection_changedMessageCommand= function(self, params)
 			if params.group then
@@ -168,7 +168,7 @@ local frame= Def.ActorFrame{
 	},
 	Def.BitmapText{
 		Name= "bpm", Font= "Common Fallback Font", InitCommand= function(self)
-			self:xy(bpm_x, bpm_y):horizalign(right):zoom(.75):diffuse(color("#451515"))
+			self:xy(bpm_x, bpm_y):horizalign(right):zoom(.75):diffuse(color("#451515")):shadowlength(1)
 		end,
 		edit_menu_selection_changedMessageCommand= function(self, params)
 			if params.group then
@@ -186,7 +186,7 @@ local frame= Def.ActorFrame{
 	},
 	Def.BitmapText{
  		Name= "title", Font= "Common Fallback Font", InitCommand= function(self)
-			self:xy(title_x, title_y):horizalign(left):zoom(.75):diffuse(color("#451515"))
+			self:xy(title_x, title_y):horizalign(left):zoom(.75):diffuse(color("#451515")):shadowlength(1)
 		end,
 		edit_menu_selection_changedMessageCommand= function(self, params)
 			if params.group then
@@ -199,7 +199,7 @@ local frame= Def.ActorFrame{
 	},
 	Def.BitmapText{
  		Name= "artist", Font= "Common Fallback Font", InitCommand= function(self)
-			self:xy(artist_x, artist_y):horizalign(left):zoom(.75):diffuse(color("#2E0F0F"))
+			self:xy(artist_x, artist_y):horizalign(left):zoom(.75):diffuse(color("#2E0F0F")):shadowlength(1)
 		end,
 		edit_menu_selection_changedMessageCommand= function(self, params)
 			if params.group then
