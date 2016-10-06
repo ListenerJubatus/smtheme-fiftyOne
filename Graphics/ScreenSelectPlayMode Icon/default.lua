488da72b6cf2e35f13c92ce26413c47337621ad6
C.C <riakodoadam@yahoo.co.uk>
2016-10-06 18:07:52 +0100

    prototype ScreenSelectPlayMode
    I'll probably leave the exact aesthetics up to everyone else

--- Graphics/ScreenSelectPlayMode Icon/default.lua
+++ Graphics/ScreenSelectPlayMode Icon/default.lua
@@ -3,33 +3,35 @@ local gc = Var("GameCommand");
 local string_name = gc:GetText()
 local string_expl = THEME:GetString(Var "LoadingScreen", gc:GetName().."Explanation")
 local icon_color = ModeIconColors[gc:GetName()];
+local icon_size = 192
 
 local t = Def.ActorFrame {};
 t[#t+1] = Def.ActorFrame {
-	GainFocusCommand=cmd(stoptweening;bob;effectmagnitude,0,6,0;decelerate,0.05;zoom,1);
-	LoseFocusCommand=cmd(stoptweening;stopeffect;decelerate,0.1;zoom,0.6);
+	GainFocusCommand=cmd(stoptweening;bob;effectmagnitude,0,0,3;decelerate,0.1;zoom,0.95);
+	LoseFocusCommand=cmd(stoptweening;stopeffect;decelerate,0.1;zoom,0.9);
 
-	LoadActor("_background base")..{
-		InitCommand=cmd(diffuse,icon_color);
+	Def.Quad {
+		InitCommand=cmd(zoomto,icon_size,icon_size;rotationz,45);
+		DisabledCommand=cmd(diffuse,ColorMidTone(icon_color);diffuselowerright,ColorDarkTone(icon_color));
+		EnabledCommand=cmd(diffuse,icon_color;diffuselowerright,ColorMidTone(icon_color));
 	};
-	LoadActor("_background effect");
-	LoadActor("_gloss");
-	LoadActor("_stroke");
-	LoadActor("_cutout");
+	--LoadActor("_background effect");
+	--LoadActor("_gloss");
+	--LoadActor("_stroke");
+	--LoadActor("_cutout");
+	
+	--Not in use yet!
+	--LoadActor( gc:GetName() ) .. {}
 
 	-- todo: generate a better font for these.
 	LoadFont("Common Large")..{
 		Text=string.upper(string_name);
-		InitCommand=cmd(y,-12;maxwidth,232);
-		OnCommand=cmd(diffuse,Color.Black;shadowlength,1;shadowcolor,color("#ffffff77");skewx,-0.125);
+		InitCommand=cmd(x,icon_size/3.4;y,icon_size/3.4;zoom,0.6;maxwidth,icon_size*1.4);
+		OnCommand=cmd(rotationz,-45;diffuse,Color.White;strokecolor,Color.Outline);
 	};
-	LoadFont("Common Normal")..{
-		Text=string.upper(string_expl);
-		InitCommand=cmd(y,27.5;maxwidth,232);
-	};
-	LoadActor("_background base") .. {
-		DisabledCommand=cmd(diffuse,color("0,0,0,0.5"));
-		EnabledCommand=cmd(diffuse,color("1,1,1,0"));
-	};
+	-- LoadFont("Common Normal")..{
+		-- Text=string.upper(string_expl);
+		-- InitCommand=cmd(y,27.5;maxwidth,232);
+	-- };
 };
 return t
