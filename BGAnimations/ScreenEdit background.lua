763faeec0d72cfbc6a684a211e2839a7ed89d66f
ListenerJubatus <superfox436@gmail.com>
2016-10-06 23:20:31 -0600

    edit mode tweaks

--- BGAnimations/ScreenEdit background.lua
+++ BGAnimations/ScreenEdit background.lua
@@ -3,13 +3,13 @@ local t = Def.ActorFrame {};
 t[#t+1] = Def.ActorFrame {
   FOV=90;
   InitCommand=cmd(Center);
-	LoadActor (GetSongBackground()) .. {
-		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
-		OnCommand=cmd(diffuse,color("#AA0055"));
-	};
 	Def.Quad {
 		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
-		OnCommand=cmd(diffuse,ColorMidTone(color("#947B7E"));diffusebottomedge,ColorMidTone(color("#D698A0"));diffusealpha,0.9);
+		OnCommand=cmd(diffuse,ColorMidTone(color("#451A20"));diffusebottomedge,ColorMidTone(color("#5E2A30"));diffusealpha,0.9);
+	};
+	LoadActor (GetSongBackground()) .. {
+		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
+		OnCommand=cmd(diffusealpha,0.1;);
 	};
 };
 
