cb5d921ed34b0734cc0684242479d100412b3c1e
ListenerJubatus <superfox436@gmail.com>
2016-10-06 19:43:38 -0600

    add stroke to gameplay song display

--- BGAnimations/ScreenGameplay decorations/default.lua
+++ BGAnimations/ScreenGameplay decorations/default.lua
@@ -13,43 +13,4 @@ for i, pn in pairs(PlayerNumber) do
 	end
 end
 
-t[#t+1] = LoadFont("TextBanner text") .. {
-          InitCommand=cmd(x,SCREEN_CENTER_X;shadowlength,1;y,SCREEN_BOTTOM-60;maxwidth,360;);
-		  OnCommand=cmd(diffusealpha,0;sleep,1.3;smooth,0.6;diffusealpha,1;);
-		  OffCommand=cmd(sleep,1;decelerate,1.4;diffusealpha,0;);
-          CurrentSongChangedMessageCommand=cmd(finishtweening;playcommand,"Set");
-          CurrentCourseChangedMessageCommand=cmd(finishtweening;playcommand,"Set");
-          ChangedLanguageDisplayMessageCommand=cmd(finishtweening;playcommand,"Set");
-          SetCommand=function(self)
-               local song = GAMESTATE:GetCurrentSong();
-               if song then
-                    self:settext(song:GetDisplayFullTitle());
-                    self:playcommand("Refresh");
-				else
-					self:settext("");
-					self:playcommand("Refresh");
-               end
-          end;
-};
-
-t[#t+1] = LoadFont("TextBanner text") .. {
-          InitCommand=cmd(zoom,0.75;shadowlength,1;x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-36;maxwidth,350;);
-		  OnCommand=cmd(diffusealpha,0;sleep,1.3;smooth,0.6;diffusealpha,1;);
-		  OffCommand=cmd(sleep,1;decelerate,1.4;diffusealpha,0;);
-          CurrentSongChangedMessageCommand=cmd(finishtweening;playcommand,"Set");
-          CurrentCourseChangedMessageCommand=cmd(finishtweening;playcommand,"Set");
-          ChangedLanguageDisplayMessageCommand=cmd(finishtweening;playcommand,"Set");
-          SetCommand=function(self)
-               local song = GAMESTATE:GetCurrentSong();
-               if song then
-                    self:settext(song:GetDisplayArtist());
-                    self:playcommand("Refresh");
-				else
-					self:settext("");
-					self:playcommand("Refresh");
-               end
-          end;
-};
-
-
 return t
