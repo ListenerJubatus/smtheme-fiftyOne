c574844bfcc7584bb2966f78d582b1cb4f6258c9
ListenerJubatus <superfox436@gmail.com>
2016-11-07 21:21:46 -0600

    update branding a bit

--- Graphics/ScreenTitleMenu logo/default.lua
+++ Graphics/ScreenTitleMenu logo/default.lua
@@ -1,14 +1,17 @@
 local t = Def.ActorFrame{
 	InitCommand=cmd(fov,70);
 	LoadActor("_arrow")..{
-		InitCommand=cmd(x,225;);
+		InitCommand=cmd(x,-290;zoom,0.5;);
 	};
-	LoadActor("_text");
-	LoadActor("_text")..{
-		Name="TextGlow";
-		InitCommand=cmd(blend,Blend.Add;diffusealpha,0.05;);
-		OnCommand=cmd(glowshift;effectperiod,2.5;effectcolor1,color("1,1,1,0.25");effectcolor2,color("1,1,1,1"););
+	Def.ActorFrame {
+		InitCommand=cmd(x,100);
+			LoadActor("_text");
+			LoadActor("_text")..{
+				Name="TextGlow";
+				InitCommand=cmd(blend,Blend.Add;diffusealpha,0.05;);
+				OnCommand=cmd(glowshift;effectperiod,2.5;effectcolor1,color("1,1,1,0.25");effectcolor2,color("1,1,1,1"););
+			};
+		};
 	};
-};
 
 return t;
