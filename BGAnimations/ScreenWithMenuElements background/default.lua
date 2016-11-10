995e5ba0360cd7f519b912baf6f1da567291abf2
C.C <riakodoadam@yahoo.co.uk>
2016-11-10 16:51:43 +0000

    Let's poke at Jukebox

--- BGAnimations/ScreenWithMenuElements background/default.lua
+++ BGAnimations/ScreenWithMenuElements background/default.lua
@@ -3,11 +3,11 @@ return Def.ActorFrame {
 		InitCommand=cmd(Center;zoomto,SCREEN_WIDTH,SCREEN_HEIGHT)
 	},
 	LoadActor("_maze") .. {
-		OnCommand=cmd(Center;diffuse,color("#f6784922");effectperiod,10;spin;effectmagnitude,0,0,3.75)
+		OnCommand=cmd(Center;diffuse,color("#f6784922");effectperiod,10;spin;effectmagnitude,0,0,2.2)
 	},
 	LoadActor("_tunnel1") .. {
 		InitCommand=cmd(Center;blend,'BlendMode_Add';rotationz,-20),
-		OnCommand=cmd(zoom,1.75;diffusealpha,0.12;spin;effectmagnitude,0,0,11)
+		OnCommand=cmd(zoom,1.75;diffusealpha,0.12;spin;effectmagnitude,0,0,16.5)
 	},		
 	LoadActor("_tunnel1") .. {
 		InitCommand=cmd(Center;blend,'BlendMode_Add';rotationz,-10),
@@ -15,10 +15,10 @@ return Def.ActorFrame {
 	},
 	LoadActor("_tunnel1") .. {
 		InitCommand=cmd(Center;blend,'BlendMode_Add';rotationz,0),
-		OnCommand=cmd(zoom,0.5;diffusealpha,0.06;spin;effectmagnitude,0,0,11)
+		OnCommand=cmd(zoom,0.5;diffusealpha,0.06;spin;effectmagnitude,0,0,5.5)
 	},		
 	LoadActor("_tunnel1") .. {
 		InitCommand=cmd(Center;blend,'BlendMode_Add';rotationz,-10),
-		OnCommand=cmd(zoom,0.2;diffusealpha,0.03;spin;effectmagnitude,0,0,-11)
+		OnCommand=cmd(zoom,0.2;diffusealpha,0.03;spin;effectmagnitude,0,0,-2.2)
 	}		
 }
