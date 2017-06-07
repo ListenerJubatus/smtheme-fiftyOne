735c2e4ae0b940976560f78f61ab51f316bcd9ff
ListenerJubatus <shawn.thompson306@gmail.com>
2017-06-07 00:56:58 -0600

    Lots of changes
    new banners, new logo, new main menu BG, etc.

--- BGAnimations/ScreenWithMenuElements background/default.lua
+++ BGAnimations/ScreenWithMenuElements background/default.lua
@@ -1,26 +1,43 @@
 -- You know what, I guess the "fancy UI background" theme option can be put to use.
 if ThemePrefs.Get("FancyUIBG") then
 	return Def.ActorFrame {
+		
 		LoadActor(THEME:GetPathG("common bg", "base")) .. {
 			InitCommand=cmd(Center;zoomto,SCREEN_WIDTH,SCREEN_HEIGHT)
 		},
+		
 		LoadActor("_maze") .. {
 			OnCommand=cmd(Center;diffuse,color("#f6784922");effectperiod,10;spin;effectmagnitude,0,0,2.2)
 		},
-		--
+		
+		LoadActor("_barcode") .. {
+			InitCommand=cmd(zoomto,64,1024;blend,'BlendMode_Add';x,SCREEN_LEFT+36;y,SCREEN_CENTER_Y;diffusealpha,0.08);
+			OnCommand=cmd(customtexturerect,0,0,1,1;texcoordvelocity,0,0.1);
+		};
+		LoadActor("_barcode") .. {
+			InitCommand=cmd(zoomto,64,1024;blend,'BlendMode_Add';x,SCREEN_RIGHT-36;y,SCREEN_CENTER_Y;diffusealpha,0.08);
+			OnCommand=cmd(customtexturerect,0,0,1,1;texcoordvelocity,0,0.1);
+		};
+		
 		Def.ActorFrame {
-		InitCommand=cmd(x,SCREEN_CENTER_X-40;y,SCREEN_CENTER_Y;rotationx,20;rotationy,12;diffusealpha,0.15);
-		OnCommand=cmd(queuecommand,"Animate");
-		AnimateCommand=cmd(rotationx,20;rotationy,12;smooth,20;rotationx,50;rotationy,36;smooth,20;rotationx,20;rotationy,12;queuecommand,"Animate");
-			LoadActor("_mask") .. {
-				InitCommand=cmd(blend,"BlendMode_NoEffect";zwrite,true;clearzbuffer,true);
+		OnCommand=cmd(diffusealpha,0;decelerate,1.8;diffusealpha,1;);
+			LoadActor("_tunnel1") .. {
+				InitCommand=cmd(x,SCREEN_LEFT+160;y,SCREEN_CENTER_Y;blend,'BlendMode_Add';rotationz,-20),
+				OnCommand=cmd(zoom,1.75;diffusealpha,0.14;spin;effectmagnitude,0,0,16.5)
+			};	
+			LoadActor("_tunnel1") .. {
+				InitCommand=cmd(x,SCREEN_LEFT+160;y,SCREEN_CENTER_Y;blend,'BlendMode_Add';rotationz,-10),
+				OnCommand=cmd(zoom,1.0;diffusealpha,0.12;spin;effectmagnitude,0,0,-11)
 			};
-			
-			LoadActor("_mist") .. {
-				InitCommand=cmd(ztest,5;zoomto,1200,1200;blend,'BlendMode_Add');
-				OnCommand=cmd(customtexturerect,0,0,1,1;texcoordvelocity,0.11,0.11);
+			LoadActor("_tunnel1") .. {
+				InitCommand=cmd(x,SCREEN_LEFT+160;y,SCREEN_CENTER_Y;blend,'BlendMode_Add';rotationz,0),
+				OnCommand=cmd(zoom,0.5;diffusealpha,0.10;spin;effectmagnitude,0,0,5.5)
+			};		
+			LoadActor("_tunnel1") .. {
+				InitCommand=cmd(x,SCREEN_LEFT+160;y,SCREEN_CENTER_Y;blend,'BlendMode_Add';rotationz,-10),
+				OnCommand=cmd(zoom,0.2;diffusealpha,0.08;spin;effectmagnitude,0,0,-2.2)
 			};
-		},
+	};
 	}
 else
 	return 	LoadActor(THEME:GetPathG("common bg", "base")) .. {
