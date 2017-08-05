ac58c7ffacbafc90f94157ac8152badae4ac23e2
ListenerJubatus <shawn.thompson306@gmail.com>
2017-08-05 17:07:10 -0600

    Course tweaks, icons, nameplates

--- Graphics/ScreenPlayerOptions PlayerNameplate.lua
+++ Graphics/ScreenPlayerOptions PlayerNameplate.lua
@@ -22,49 +22,37 @@ if GAMESTATE:GetCurrentSong() then
 end
 
 local t = Def.ActorFrame {
-	LoadActor(THEME:GetPathB("_frame","3x1"),"rounded fill", 250-16) .. {
-		OnCommand=cmd(diffuse,ColorDarkTone(PlayerColor(PlayerNumber));diffusealpha,0.9);
+	LoadActor(THEME:GetPathB("_frame","3x1"),"rounded light", 250-16) .. {
+		OnCommand=cmd(diffuse,ColorLightTone(PlayerColor(PlayerNumber));diffusealpha,0.7);
 	};
 	LoadFont("Common Condensed") .. {
 		Text=ToEnumShortString(PlayerNumber);
 		Name="PlayerShortName",
-		InitCommand=cmd(x,-127;maxwidth,32;zoom,0.75),
-		OnCommand=cmd(diffuse,PlayerColor(PlayerNumber);)
+		InitCommand=cmd(x,-127;maxwidth,32;zoom,0.75;shadowlength,1;),
+		OnCommand=cmd(diffuse,ColorDarkTone(PlayerColor(PlayerNumber));)
 	},
 	LoadFont("_overpass 36px") .. {
 		Text=bpm_text;
 		Name="BPMRangeOld",
 		InitCommand=cmd(x,-60;maxwidth,88/bpm_text_zoom),
-		OnCommand=cmd(zoom,bpm_text_zoom)
+		OnCommand=cmd(zoom,bpm_text_zoom;diffuse,ColorDarkTone(PlayerColor(PlayerNumber));shadowlength,1;)
 	},
 	LoadActor(THEME:GetPathG("_StepsDisplayListRow","arrow")) .. {
 		Name="Seperator",
-		InitCommand=cmd(x,4)
+		InitCommand=cmd(x,4);
+		OnCommand=cmd(diffuse,ColorDarkTone(PlayerColor(PlayerNumber));)
 	},
 	LoadFont("_overpass 36px") .. {
 		Text="100 - 200000";
+		Text="100 - 200000";
 		Name="BPMRangeNew",
 		InitCommand= function(self)
-			self:x(68):maxwidth(88/bpm_text_zoom):zoom(bpm_text_zoom)
-			self:playcommand("ConfigValueChanged", {pn= PlayerNumber, field_name= "speed_mod"})
-			--local speed, mode= GetSpeedModeAndValueFromPoptions(PlayerNumber)
-			--self:playcommand("SpeedChoiceChanged", {pn= PlayerNumber, mode= mode, speed= speed})
+			self:x(68):maxwidth(88/bpm_text_zoom):shadowlength(1):zoom(bpm_text_zoom)
+			local speed, mode= GetSpeedModeAndValueFromPoptions(PlayerNumber)
+			self:playcommand("SpeedChoiceChanged", {pn= PlayerNumber, mode= mode, speed= speed})
 		end,
-		BPMWillNotChangeCommand=cmd(stopeffect),
-		BPMWillChangeCommand=cmd(diffuseshift;effectcolor1,Color.White;effectcolor2,Color.Orange),
-		ConfigValueChangedMessageCommand= function(self, param)
-			if param.field_name == "speed_mod" or param.field_name == "speed_type" then
-				local prefs= notefield_prefs_config:get_data(param.pn)
-				local mode_conversion= {maximum= "m", multiple= "x", constant= "C"}
-				local speed= prefs.speed_mod
-				local mode= mode_conversion[prefs.speed_type]
-				if mode == "x" then
-					speed= speed * 100
-				end
-				self:playcommand(
-					"SpeedChoiceChanged", {pn= param.pn, speed= speed, mode= mode})
-			end
-		end,
+		BPMWillNotChangeCommand=cmd(stopeffect;diffuse,ColorDarkTone(PlayerColor(PlayerNumber));),
+		BPMWillChangeCommand=cmd(diffuseshift;effectcolor1,Color.Black;effectcolor2,ColorLightTone(PlayerColor(PlayerNumber));),
 		SpeedChoiceChangedMessageCommand= function(self, param)
 			if param.pn ~= PlayerNumber then return end
 			local text= ""
