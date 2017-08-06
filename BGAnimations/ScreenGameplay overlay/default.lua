53d1d14fd381841d6713a567b74a90e2555b6b2d
ListenerJubatus <shawn.thompson306@gmail.com>
2017-08-05 18:03:21 -0600

    Move lifebar code to same place as frame overlay

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,4 +1,17 @@
 local t = Def.ActorFrame {};
+
+	for i, pn in pairs(PlayerNumber) do
+		if ShowStandardDecoration("LifeMeterBar" ..  ToEnumShortString(pn)) then
+			local life_type = GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Song"):LifeSetting()
+			t[#t+1] = LoadActor(THEME:GetPathG(Var "LoadingScreen", "lifebar_" .. ToEnumShortString(life_type)), pn) .. {
+				InitCommand=function(self)
+					self:name("LifeMeterBar" .. ToEnumShortString(pn))
+					ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen")
+				end
+			}
+		end
+	end
+
 	--P1
 	if GAMESTATE:GetPlayMode() ~= 'PlayMode_Rave' then
 		if GAMESTATE:IsHumanPlayer(PLAYER_1) == true then
