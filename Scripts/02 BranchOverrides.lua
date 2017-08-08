273db82a28d7a1861bca81dabc4a11205dc884a6
ListenerJubatus <shawn.thompson306@gmail.com>
2017-08-07 21:51:08 -0600

    Revert to original edit menu

--- Scripts/02 BranchOverrides.lua
+++ Scripts/02 BranchOverrides.lua
@@ -1,27 +1,3 @@
-Branch.PlayerOptions= function()
-	local pm = GAMESTATE:GetPlayMode()
-	local restricted = { PlayMode_Oni= true, PlayMode_Rave= true,
-		--"PlayMode_Battle" -- ??
-	}
-	-- this is just here in case the nesty menus ever get adapted to 5.0.
-	-- I'm sorry for not just doing it in the first place
-	local nesty = false
-	
-	local optionsScreen = "ScreenPlayerOptions"
-	
-	if nesty then
-		optionsScreen = "ScreenNestyPlayerOptions"
-	end
-	if restricted[pm] then
-		optionsScreen = "ScreenPlayerOptionsRestricted"
-	end
-	if SCREENMAN:GetTopScreen():GetGoToOptions() then
-		return optionsScreen
-	else
-		return "ScreenStageInformation"
-	end
-end
-
 Branch.OptionsEdit = function()
 	if SONGMAN:GetNumSongs() == 0 and SONGMAN:GetNumAdditionalSongs() == 0 then
 		return "ScreenHowToInstallSongs"
